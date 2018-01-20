239c2bb7038b569b936603f70add83a53759391c
linj2n <linj2n@163.com>
2018-01-20 12:36:26 +0800

    add some more effective solutions for #657

--- 657. Judge Route Circle/657_Judge_Route_Circle.java
+++ 657. Judge Route Circle/657_Judge_Route_Circle.java
@@ -1,3 +1,5 @@
+// Approach #1 Using HashMap
+// Runtime: 48ms
 class Solution {
     public boolean judgeCircle(String moves) {
         HashMap <Character,Integer> h = new HashMap<>();
@@ -10,3 +12,37 @@ class Solution {
         return h.get('L').equals(h.get('R'))  && h.get('U').equals(h.get('D'));
     }
 }
+
+// Approach #2 Convert the String to a new char array.
+// Runtime: 15ms
+class Solution {
+    public boolean judgeCircle(String moves) {
+        char [] strArr = moves.toCharArray();   
+        int v = 0, h = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i] == 'L') {
+                h ++;
+            }else if (strArr[i] == 'R') {
+                h --;
+            }else if (strArr[i] == 'U') {
+                v ++;
+            }else if (strArr[i] == 'D') {
+                v --;
+            }
+        }
+        return v == 0 && h == 0;
+    }
+
+}
+// Approach #3 Use extra array that storing the ocurr times and check if their value are eqaul or not. 
+// Runtime: 9ms
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int [] temp = new int[128];
+        for (char c: moves.toCharArray()) {
+            temp[c] ++;
+        }
+        return temp['L'] == temp['R'] && temp['U'] == temp['D'];
+    }
+}
+
