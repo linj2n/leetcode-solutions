8adf379f42e868688652438ee13d1d06322cda0f
linj2n <linj2n@163.com>
2018-01-20 19:00:54 +0800

    add #136 solution

---
+++ 136. Single Number/136_Single_Number.java
@@ -0,0 +1,10 @@
+// Approach #1 XOR bit operation
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
