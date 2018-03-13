7a4f30c0438532e498cd7fb439c5c71d94371a5d
linj2n <linj2n@163.com>
2018-03-13 11:00:48 +0800

    add extra solution

--- 205. Isomorphic Strings/205_Isomorphic_Strings.java
+++ 205. Isomorphic Strings/205_Isomorphic_Strings.java
@@ -1,6 +1,6 @@
 // Approach #1 
 // Runtime: 23 ms
-class Solution {
+class Solution1 {
     public String convertToPatternStr(String s) {
         HashMap <Character,Integer>sh = new HashMap<>();
         StringBuilder sb = new StringBuilder();
@@ -19,4 +19,39 @@ class Solution {
         String str2 = convertToPatternStr(t);
         return str1.equals(str2);
     }
+}
+
+// Approach #1 
+// Runtime: 13 ms
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if (s.length() != t.length() ) return false;
+        else if(s.length() == 0) return true;
+        
+        int[] charValueOfS = new int[128];
+        int[] charValueOfT = new int[128];
+        boolean resFlag = true;
+        
+        int[] patternArrayOfS = new int[s.length()];
+        int[] patternArrayOfT = new int[t.length()];
+        int patternNumOfS = 1,patternNumOfT = 1,arrayLen = s.length();
+        for (int i = 0; i < arrayLen; i++) {
+            char currCharOfS = s.charAt(i);
+            char currCharOfT = t.charAt(i);
+            if (charValueOfS[currCharOfS] == 0) {
+                charValueOfS[currCharOfS] = patternNumOfS++;
+            } 
+            patternArrayOfS[i] = charValueOfS[currCharOfS];
+            
+            if (charValueOfT[currCharOfT] == 0) {
+                charValueOfT[currCharOfT] = patternNumOfT++;
+            } 
+            patternArrayOfT[i] = charValueOfT[currCharOfT];
+            if (patternArrayOfS[i] != patternArrayOfT[i]) {
+                resFlag = false; break;
+            } 
+        }
+        return resFlag;
+    } 
 }
