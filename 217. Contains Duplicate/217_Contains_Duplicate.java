e40c38c6b7987ebbbe8cf419985e533f3cc4bfa7
linj2n <linj2n@163.com>
2018-03-17 10:41:02 +0800

    add extra solution and shorten code

--- 217. Contains Duplicate/217_Contains_Duplicate.java
+++ 217. Contains Duplicate/217_Contains_Duplicate.java
@@ -1,11 +1,23 @@
 // Approach #1 
+// 13ms
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if (nums.length == 0) return false;
-        Set hs = new HashSet();
-        for (int i: nums) {
-            if (hs.contains(i)) return true;
-            hs.add(i);
+        Set<Integer> set = new HashSet<Integer>();
+        for (int e: nums) {
+            if (!set.add(e))	return true;
+        }
+        return false;
+    }
+}
+
+// Approach #2
+// 6ms
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) { 
+            if (nums[i] == nums[i+1]) return true;
         }
         return false;
     }
