09d46e430cd0c8701d6a19189b5adbf7de23f81f
linj2n <linj2n@163.com>
2018-09-19 13:46:28 +0800

    :ambulance: Add correct solution in java

--- java/src/main/java/_27/RemoveElement.java
+++ java/src/main/java/_27/RemoveElement.java
@@ -1,13 +1,14 @@
 package _27;
+
 public class RemoveElement {
-    public int removeDuplicates(int[] nums) {
+    public int removeElement(int[] nums, int val) {
         if (nums == null || nums.length == 0) return 0;
-        int lastIndex = 0;
-        for (int walk = lastIndex + 1; walk < nums.length; walk ++) {
-            if (walk < nums.length && nums[lastIndex] == nums[walk]) {
+        int lastIndex = -1;
+        for (int walk = lastIndex +1; walk < nums.length; walk++) {
+            if (nums[walk] == val) {
                 continue;
             }
-            nums[++ lastIndex] = nums[walk];
+            nums[++lastIndex] = nums[walk];
         }
         return lastIndex + 1;
     }
