0f1b9f7ef56360b55b9d02409d97ada3c6783d27
linj2n <linj2n@163.com>
2018-09-20 15:47:21 +0800

    :tada: Init Solution #3

---
+++ java/src/main/java/_3/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,49 @@
+class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        return solution1(s);
+    }
+    private int solution1(String s) {
+        Set<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int i = 0, j = 0;
+        while (i < s.length() && j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                // maxLength = Math.max(maxLength,set.size());
+                maxLength = Math.max(maxLength,j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return maxLength;
+    }
+    private int solution2(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int i = 0, j = 0;
+        while (i < s.length() && j < s.length()) {
+            char c = s.charAt(j);
+            if (map.containsKey(c)) {
+                i = Math.max(map.get(c) + 1,i);
+            }
+            map.put(c,j++);
+            maxLength = Math.max(maxLength,j - i);
+        }
+        return maxLength;
+    }
+    private int solution3(String s) {
+        int[] set = new int[128];
+        int maxLength = 0;
+        int i = 0, j = 0;
+        while (i < s.length() && j < s.length()) {
+            Boolean exited = (set[s.charAt(j)] == 1);
+            if (!exited) {
+                set[s.charAt(j++)] = 1;
+                maxLength = Math.max(maxLength,j - i);
+            } else {
+                set[s.charAt(i++)] = 0;
+            }
+        }
+        return maxLength;
+    }
+}
