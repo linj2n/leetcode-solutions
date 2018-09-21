f7c7b9c3f270dffe957b5e50bca603344244a714
linj2n <linj2n@163.com>
2018-09-21 09:14:51 +0800

    :fire: Polish Solution #28

--- java/src/main/java/_28/Implement_strStr.java
+++ java/src/main/java/_28/Implement_strStr.java
@@ -2,30 +2,24 @@ package _28;
 
 public class implement_strStr {
     public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) {
+            return -1;
+        } 
         if (needle.length() == 0) {
             return 0;
         }
-        if (haystack.length() < needle.length()) {
-            return -1;
-        }
-        if (haystack.length() == needle.length()) {
-            return haystack.equals(needle) ? 0 : -1;
-        }
-        int i = 0; 
-        while (i < haystack.length()) {
-            int j = i, k = 0;
-            while (k < needle.length() && j < haystack.length() && haystack.charAt(j) == needle.charAt(k)) {
-                k ++;
-                j ++;
+        int beginIndex = 0; 
+        int hayLen = haystack.length(), needleLen = needle.length();
+        while ((hayLen - beginIndex) >= needleLen) {
+            int j = beginIndex, k = 0;
+            while (k < needleLen && haystack.charAt(j) == needle.charAt(k)) {
+                j ++; k++;
             }
-            if (k == needle.length()) {
-                return i;
+            if (k == needleLen) {
+                return beginIndex;
             }
-            if (j == haystack.length()) {
-                return -1;
-            }            
-            i ++;
+            beginIndex ++;
         }
-        return (i >= haystack.length()) ? -1 : i;
+        return -1;
     }
 }
