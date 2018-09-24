4d81f443af4c86d4b0af63a84bb255baff310dc0
linj2n <linj2n@163.com>
2018-09-24 17:47:41 +0800

    :tada: Init Solution #67

---
+++ java/src/main/java/_67/AddBinary.java
@@ -0,0 +1,25 @@
+ package _67;
+
+ import java.util.*;
+ public class AddBinary {
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1, j = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += (a.charAt(i --) - '0');
+            }
+            if (j >= 0) {
+                sum += (b.charAt(j --) - '0');
+            }
+            carry = sum / 2;
+            sb.append(sum % 2);
+        }
+        if (carry != 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
