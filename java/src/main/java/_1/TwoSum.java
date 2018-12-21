a25a5cec84028ec9870beda0ef1cebdab9417da8
linj2n <linj2n@163.com>
2018-12-21 23:26:42 +0800

    🚧 Update
    NONE #31

--- java/src/main/java/_1/TwoSum.java
+++ java/src/main/java/_1/TwoSum.java
@@ -8,17 +8,13 @@ public class TwoSum {
     }
     private int[] solution1(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i ++) {
+        for (int i = 0; i < nums.length; i++) {
+            int supplement = target - nums[i];
+            if (map.containsKey(supplement)) {
+                return new int[]{map.get(supplement),i};
+            }
             map.put(nums[i],i);
         }
-        List<Integer> list = new ArrayList<>();
-        for (int j = 0; j < nums.length; j ++) {
-            if (map.containsKey(target - nums[j]) && map.get(target - nums[j]) != j) {
-                list.add(j);
-                list.add(map.get(target - nums[j]));
-                break;
-            }
-        }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return null;
     }
 }
