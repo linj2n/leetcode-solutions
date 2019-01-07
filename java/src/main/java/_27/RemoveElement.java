c61c2059aeb914febcc0ff0bba21f6ecde5d957b
linj2n <linj2n@163.com>
2019-01-07 20:50:24 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_27/RemoveElement.java
+++ java/src/main/java/_27/RemoveElement.java
@@ -2,13 +2,12 @@ package _27;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        if (nums == null || nums.length == 0) return 0;
-        int lastIndex = -1;
-        for (int walk = lastIndex +1; walk < nums.length; walk++) {
+        int endIndex = 0;
+        for (int walk = endIndex; walk < nums.length; ++ walk) {
             if (nums[walk] != val) {
-                nums[++lastIndex] = nums[walk];
+                nums[endIndex ++] = nums[walk];
             }
         }
-        return lastIndex + 1;
+        return endIndex;
     }
 }
