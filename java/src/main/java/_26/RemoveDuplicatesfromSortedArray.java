19a6e25632a82ccaa39f9fa10ee1e15959adaad0
linj2n <linj2n@163.com>
2019-01-07 20:42:22 +0800

    🚧 Polish
    NONE #31

--- java/src/main/java/_26/RemoveDuplicatesfromSortedArray.java
+++ java/src/main/java/_26/RemoveDuplicatesfromSortedArray.java
@@ -2,9 +2,11 @@ package _26;
 
 public class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int lastIndex = 0;
-        for (int walk = lastIndex + 1; walk < nums.length; walk ++) {
+        for (int walk = lastIndex; walk < nums.length; ++ walk) {
             if (nums[lastIndex] != nums[walk]) {
                 nums[++ lastIndex] = nums[walk];   
             }
