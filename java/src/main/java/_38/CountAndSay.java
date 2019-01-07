24e4c9acdfcc18549d8e7cd777a27595e36bef6a
linj2n <linj2n@163.com>
2019-01-07 18:25:29 +0800

    🎨 Polish and add new solution
    NONE #31

--- java/src/main/java/_38/CountAndSay.java
+++ java/src/main/java/_38/CountAndSay.java
@@ -4,11 +4,11 @@ import java.util.StringBuilder;
 
 public class CountAndSay {
     public String countAndSay(int n) {
+        return solution1(n);
+    }
+    priavte String solution1(int n) {
         int i = 0;
         String str = "1";
-        if (n == 1) {
-            return str;
-        }
         while (i < n - 1) {
             StringBuilder sb = new StringBuilder();
             int j = 0;
@@ -18,12 +18,34 @@ public class CountAndSay {
                     count ++;
                     k ++;
                 }
-                sb.append(String.valueOf(count)).append(str.charAt(j));
+                sb.append(count).append(str.charAt(j));
                 j = k;
             }
             str = sb.toString();
             i ++;
         }
         return str;
+    }
+
+    private String solution2(int n) {
+        String res = "1";
+        for (int i = 1; i < n; i ++) {
+            StringBuilder sb = new StringBuilder();
+            int k = 0, say = Character.digit(res.charAt(k),10), count = 0;
+            while (k < res.length()) {
+                int digit = Character.digit(res.charAt(k),10);
+                if (digit == say) {
+                    ++ count;
+                } else {
+                    sb.append(count).append(say);
+                    say = digit;
+                    count = 1;
+                }
+                ++ k;
+            }
+            sb.append(count).append(say);
+            res = sb.toString();
+        }
+        return res;
     }
 }
