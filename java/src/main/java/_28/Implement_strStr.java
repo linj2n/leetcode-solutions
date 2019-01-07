54758c8a5ea422e6be9a6bea034352105f5074b0
linj2n <linj2n@163.com>
2019-01-07 16:10:47 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_28/Implement_strStr.java
+++ java/src/main/java/_28/Implement_strStr.java
@@ -2,24 +2,23 @@ package _28;
 
 public class implement_strStr {
     public int strStr(String haystack, String needle) {
-        if (haystack == null || needle == null) {
-            return -1;
-        } 
         if (needle.length() == 0) {
             return 0;
         }
+        if (haystack.length() == needle.length()) {
+            return haystack.equals(needle) ? 0 : -1;
+        }
         int beginIndex = 0; 
-        int hayLen = haystack.length(), needleLen = needle.length();
-        while ((hayLen - beginIndex) >= needleLen) {
+        while ((haystack.length() - beginIndex) >= needle.length()) {
             int j = beginIndex, k = 0;
-            while (k < needleLen && haystack.charAt(j) == needle.charAt(k)) {
+            while (k < needle.length() && haystack.charAt(j) == needle.charAt(k)) {
                 j ++; k++;
             }
-            if (k == needleLen) {
+            if (k == needle.length()) {
                 return beginIndex;
             }
             beginIndex ++;
         }
         return -1;
-    }
+    }   
 }
