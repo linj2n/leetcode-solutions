6d459be70e91e3968624dff7745f70909a0634b2
linj2n <linj2n@163.com>
2019-01-07 21:24:16 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_35/SearchInsertPosition.java
+++ java/src/main/java/_35/SearchInsertPosition.java
@@ -10,21 +10,20 @@ class SearchInsertPosition {
         for (; i < nums.length && target > nums[i]; i ++) {}
         return i; 
     }
+    // bug free edition
     private int solution2(int[] nums, int target) {
-        if (nums == null) return -1;
-        int beg = 0, end = nums.length - 1;
-        while (beg <= end) {
-            int mid = beg + (end - beg) / 2;
-            if (nums[mid] == target) {
+        int first = 0, last = nums.length;
+        while (first < last) {
+            int mid = first + (last - first) / 2;
+            if (target == nums[mid]) {
                 return mid;
-            }
-            if (nums[mid] > target) {
-                end = mid - 1;
+            } else if (target > nums[mid]) {
+                first = mid + 1;
             } else {
-                beg = mid + 1;
-            } 
+                last = mid;
+            }
         }
-        return beg;
+        return first;
     }
     private int solution3(int[] nums, int target) {
         if (nums == null) return -1;
