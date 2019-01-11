c9fe0b08c167d8d70bbcb5bbac3027828f26e63e
linj2n <linj2n@163.com>
2019-01-11 15:29:09 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_167/TwoSumII.java
+++ java/src/main/java/_167/TwoSumII.java
@@ -1,19 +1,22 @@
 package _167;
 
+import java.util.Arrays;
+
 public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
         return solution1(numbers, target);
     }
+
     private int[] solution1(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1, sum;
         while (left < right) {
             sum = numbers[left] + numbers[right];
             if (sum == target) {
                 return new int[]{left + 1, right + 1};
-            } else if (sum > target){
-                -- right;
+            } else if (sum > target) {
+                --right;
             } else {
-                ++ left;
+                ++left;
             }
         }
         return null;
