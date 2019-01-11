fe30f68530c398d2dd98415de26b38c83133f754
linj2n <linj2n@163.com>
2019-01-11 09:50:36 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_118/PascalsTriangle.java
+++ java/src/main/java/_118/PascalsTriangle.java
@@ -5,18 +5,19 @@ import java.util.List;
 
 public class PascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> rows = new ArrayList<>();
-        for (int i = 1; i <= numRows; ++ i) {
-            List<Integer> row = new ArrayList<>(i);
-            row.add(1);
+        if (numRows < 0) {
+            return null;
+        }
+        List<List<Integer>> rows = new ArrayList<>(numRows);
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i ++) {
             if (!rows.isEmpty()) {
-                List<Integer> preRow = rows.get(rows.size() - 1);
-                for (int j = 0; j < preRow.size() - 1; ++ j) {
-                    row.add(preRow.get(j) + preRow.get(j + 1));
+                for (int j = row.size() - 1; j > 0; -- j) {
+                    row.set(j,row.get(j) + row.get(j - 1));
                 }
-                row.add(1);
             }
-            rows.add(row);
+            row.add(1);
+            rows.add(new ArrayList<>(row));
         }
         return rows;
     }
