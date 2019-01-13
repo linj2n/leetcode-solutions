7bd6d0c17461c7086c2c06802a2788b361b3a6e5
linj2n <linj2n@163.com>
2019-01-13 15:09:42 +0800

    ✨ Add new solutions
    NONE #31

--- java/src/main/java/_219/ContainsDuplicateII.java
+++ java/src/main/java/_219/ContainsDuplicateII.java
@@ -1,9 +1,10 @@
 package _219;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
-// Approach #1
 public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         return solution1(nums, k);
@@ -18,6 +19,32 @@ public class ContainsDuplicateII {
                 return true;
             }
             indexByValue.put(nums[i],i);
+        }
+        return false;
+    }
+
+    // solution 2: Using hash set
+    public boolean solution2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++ i) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // solution 3: Brute force
+    public boolean solution3(int[] nums, int k) {
+        for (int i = 0; i < nums.length; ++ i) {
+            for (int j = i + 1; j < nums.length && j <= i + k; ++ j) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
         }
         return false;
     }
