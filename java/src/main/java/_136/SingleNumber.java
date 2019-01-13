e377e91c30d9feb1bc063a57e583b008b7415ce3
linj2n <linj2n@163.com>
2019-01-13 17:26:14 +0800

    ✨ Add new solution
    NONE #31

--- java/src/main/java/_136/SingleNumber.java
+++ java/src/main/java/_136/SingleNumber.java
@@ -1,12 +1,35 @@
 package _136;
 
-// Approach #1 XOR bit operation
+import java.util.HashMap;
+import java.util.Map;
+
 class SingleNumber {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            ans = ans ^ nums[i];
+        return solution2(nums);
+    }
+
+    // solution 1: Bit manipulation[xor]
+    private int solution1(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
         }
-        return ans;
+        return single;
+    }
+
+    // solution 2: Using HashTable
+    private int solution2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            Integer count = map.get(num);
+            map.put(num,(count == null) ? 1 : count + 1);
+        }
+    
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 }
