da12908eab158b66a9d62210c28b8c27303bfe1d
linj2n <linj2n@163.com>
2019-01-19 16:08:15 +0800

    🎉 Init solution #105
    NONE #31

---
+++ java/src/main/java/_105/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,29 @@
+package _105;
+
+import support.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+        Map<Integer, Integer> valToIndex = new HashMap<>();
+        for (int i = 0; i < inorder.length; i ++) {
+            valToIndex.put(inorder[i],i);
+        }
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, valToIndex);
+    }
+    private TreeNode buildTree(int[] preorder, int preFirst, int preLast, int[] inorder, int inFirst, int inLast, Map<Integer, Integer> valToIndex) {
+        if (preFirst > preLast) {
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[preFirst]);
+        int index = valToIndex.get(preorder[preFirst]);
+        node.left = buildTree(preorder, preFirst + 1, preFirst + index - inFirst, inorder, inFirst, index - 1, valToIndex);
+        node.right = buildTree(preorder, preFirst + index - inFirst + 1, preLast, inorder, index + 1, inLast, valToIndex);
+        return node;
+    }
+}
