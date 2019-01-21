70ea2ff0749017e2fbc0e33557078435f5d527ee
linj2n <linj2n@163.com>
2019-01-21 20:04:41 +0800

    ✨ Add the new solution
    NONE #31

--- java/src/main/java/_110/BalancedBinaryTree.java
+++ java/src/main/java/_110/BalancedBinaryTree.java
@@ -4,7 +4,7 @@ import support.TreeNode;
 
 public class BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
-        return solution1(root);
+        return solution2(root);
     }
 
     // solution 1: Brute force
@@ -26,5 +26,32 @@ public class BalancedBinaryTree {
         int leftDepth = treeDepth(root.left);
         int rightDepth = treeDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    // solution 2: Postorder traversal
+    private boolean solution2(TreeNode root) {
+        boolean[] result = new boolean[1];
+        result[0] = true;
+        treeDepth(root, 0, result);
+        return result[0];
+    }
+    private int treeDepth(TreeNode root, int level, boolean[] result) {
+        if (root == null) {
+            return level;
+        }
+        int leftHeight = treeDepth(root.left, level + 1, result);
+        if (result[0] == false) {
+            return level;
+        }
+
+        int rightHeight = treeDepth(root.right, level + 1, result);
+        if (result[0] == false) {
+            return level;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            result[0] = false;
+        }
+        return Math.max(leftHeight, rightHeight);
     }
 }
