8f3485896fdf43400151c70967d9859066ace6ec
linj2n <linj2n@163.com>
2019-01-26 15:17:21 +0800

    🎉 Init solution #83
    NONE #31

---
+++ java/src/main/java/_83/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,21 @@
+package _83;
+
+import support.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode l1 = head, l2 = head.next;
+        while (l1 != null && l2 != null) {
+            while (l2 != null && l1.val == l2.val) {
+                l2 = l2.next;
+            }
+            l1.next = l2;
+            l1 = (l2 != null) ? l2 : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return head;
+    }
+}
