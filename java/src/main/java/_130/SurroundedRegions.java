d7ee9349f44cc153da67e49ec3a0a3a5ea989109
linj2n <linj2n@163.com>
2019-01-28 20:44:41 +0800

    🎉 Init solution #130
    NONE #31

--- java/src/main/java/_130/SurroundedRegions.java
+++ java/src/main/java/_130/SurroundedRegions.java
@@ -1,6 +1,8 @@
 package _130;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class SurroundedRegions {
@@ -10,11 +12,10 @@ public class SurroundedRegions {
         if (board == null || board.length == 0 || board[0].length == 0) {
             return ;
         }
-        solution1(board);
+        helper(board);
     }
 
-    // DFS
-    private void solution1(char[][] board) {
+    private void helper(char[][] board) {
         boolean[][] notCaptured = new boolean[board.length][board[0].length];
         List<Integer> xList = new ArrayList<>(), yList = new ArrayList<>();
         int height = board.length, width = board[0].length;
@@ -42,23 +43,45 @@ public class SurroundedRegions {
                 notCaptured[height - 1][j] = true;
             }
         }
+        // System.out.println(xList.size() + ", " + yList.size());
         for (int k = 0; k < xList.size(); k ++) {
-            dfs(board, notCaptured, xList.get(k), yList.get(k));
+            bfs(board, notCaptured, xList.get(k), yList.get(k));
         }
         for (int sr = 0; sr < height; sr ++) {
             for (int sc = 0; sc < width; sc ++) {
-                if (board[sr][sc] == 'O' && !notCaptured[sr][sc]) {
+                if (!notCaptured[sr][sc]) {
                     board[sr][sc] = 'X';
                 }
             }
         }
     }
+
+    // DFS
     private void dfs(char[][] board, boolean[][] notCaptured, int row, int col) {
         notCaptured[row][col] = true;
         for (int i = 0; i < 4; i ++) {
             int nRow = row + dx[i], nCol = col + dy[i];
             if (nRow >= 0 && nRow < board.length && nCol >= 0 && nCol < board[0].length && !notCaptured[nRow][nCol] && board[nRow][nCol] == 'O') {
                 dfs(board, notCaptured, nRow, nCol);
+            }
+        }
+    }
+
+    // BFS
+    private void bfs(char[][] board, boolean[][] notCaptured, int row, int col) {
+        notCaptured[row][col] = true;
+        Deque<Integer> xDeque = new ArrayDeque<>(), yDeque = new ArrayDeque<>();
+        xDeque.addLast(row);
+        yDeque.addLast(col);
+        while (!xDeque.isEmpty()) {
+            int x = xDeque.removeFirst(), y = yDeque.removeFirst();
+            for (int i = 0; i < 4; i ++) {
+                int nRow = x + dx[i], nCol = y + dy[i];
+                if (nRow >= 0 && nRow < board.length && nCol >= 0 && nCol < board[0].length && !notCaptured[nRow][nCol] && board[nRow][nCol] == 'O') {
+                    xDeque.addLast(nRow);
+                    yDeque.addLast(nCol);
+                    notCaptured[nRow][nCol] = true;
+                }
             }
         }
     }
