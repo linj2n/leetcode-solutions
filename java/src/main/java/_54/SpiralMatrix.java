1ca4e021a200d9426811f908dda3def4a2a5eb7d
linj2n <linj2n@163.com>
2019-01-29 16:55:56 +0800

    ✨ Add new solutions
    NONE #31

--- java/src/main/java/_54/SpiralMatrix.java
+++ java/src/main/java/_54/SpiralMatrix.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class SpiralMatrix {
     int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
     public List<Integer> spiralOrder(int[][] matrix) {
+        return solution1(matrix);
+    }
+    private List<Integer> solution1(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return list;
@@ -27,6 +30,33 @@ public class SpiralMatrix {
                     nSc += dy[t];
                 }
             }
+        }
+        return list;
+    }
+    private List<Integer> solution2(int[][] matrix) {
+        int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+        List<Integer> list = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return list;
+        }
+        int height = matrix.length, width = matrix[0].length;
+        boolean[][] isVisited = new boolean[height][width];
+        int x = 0, y = 0, d = 0;
+        int nextX = 0, nextY = 0;
+        for (int i = 0; i < height * width; i ++) {
+
+            list.add(matrix[x][y]);
+            isVisited[x][y] = true;
+
+            nextX = x + dx[d];
+            nextY = y + dy[d];
+            if (nextX < 0 || nextX >= height || nextY < 0 || nextY >= width || isVisited[nextX][nextY]) {
+                d = (d + 1) % 4;
+                nextX = x + dx[d];
+                nextY = y + dy[d];
+            }
+            x = nextX;
+            y = nextY;
         }
         return list;
     }
