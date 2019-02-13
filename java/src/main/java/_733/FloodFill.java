97f923ab51d299dcc2c955cc8f6eff23cf9d0aaa
linj2n <linj2n@163.com>
2019-02-13 09:16:36 +0800

    🐛 Fix a bug
    NONE #31

--- java/src/main/java/_733/FloodFill.java
+++ java/src/main/java/_733/FloodFill.java
@@ -10,31 +10,36 @@ import java.util.Deque;
 */
 
 public class FloodFill {
-    int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+    private int[] dRow = {-1, 0, 1, 0}, dCol = {0, 1, 0, -1};
+    private int rowLength, colLength;
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if (image == null || image.length == 0 || image[0].length == 0 || image[sr][sc] == newColor) {
+        if (image == null || image.length == 0 || image[0].length == 0) {
             return image;
         }
-        int height = image.length, width = image[0].length;
-        bfs(image, sr, sc, newColor);
+        this.rowLength = image.length;
+        this.colLength = image[0].length;
+        dfs(image, sr, sc, newColor);
         return image;
     }
 
     // DFS
-    private void dfs(int[][] image, int row, int col, int newColor) {
-        int oldColor = image[row][col];
-        image[row][col] = newColor;
+    private void dfs(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) {
+            return ;
+        }
+        int oldColor = image[sr][sc];
+        image[sr][sc] = newColor;
         for (int i = 0; i < 4; i ++) {
-            int nRow = row + dx[i], nCol = col + dy[i];
-            if (nRow >= 0 && nRow< image.length && nCol >= 0 && nCol < image[0].length && image[nRow][nCol] == oldColor) {
-                dfs(image, nRow, nCol, newColor);
+            int nSr = sr + dRow[i], nSc = sc + dCol[i];
+            if (nSr >= 0 && nSr < rowLength && nSc >= 0 && nSc < colLength && image[nSr][nSc] == oldColor) {
+                dfs(image, nSr, nSc, newColor);
             }
         }
     }
 
     // BFS
     private void bfs(int[][]image, int row, int col, int newColor) {
-        Deque<Integer> xDeque = new ArrayDeque<Integer>(), yDeque = new ArrayDeque<Integer>();
+        Deque<Integer> xDeque = new ArrayDeque<>(), yDeque = new ArrayDeque<>();
         xDeque.addLast(row);
         yDeque.addLast(col);
         int oldColor = image[row][col];
@@ -42,8 +47,8 @@ public class FloodFill {
         while (!xDeque.isEmpty()) {
             int x = xDeque.removeFirst(), y = yDeque.removeFirst();
             for (int i = 0; i < 4; i ++) {
-                int nRow = x + dx[i], nCol = y + dy[i];
-                if (nRow >= 0 && nRow < image.length && nCol >= 0 && nCol < image[0].length && image[nRow][nCol] == oldColor) {
+                int nRow = x + dRow[i], nCol = y + dCol[i];
+                if (nRow >= 0 && nRow < rowLength && nCol >= 0 && nCol < colLength && image[nRow][nCol] == oldColor) {
                     xDeque.addLast(nRow);
                     yDeque.addLast(nCol);
                     image[nRow][nCol] = newColor;
