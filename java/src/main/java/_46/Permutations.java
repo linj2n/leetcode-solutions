60e9130c6c843e1c6bce7e35396c1345e7b890ef
linj2n <linj2n@163.com>
2019-02-13 08:25:44 +0800

    🎨 Polish
    NONE #31

--- java/src/main/java/_46/Permutations.java
+++ java/src/main/java/_46/Permutations.java
@@ -7,22 +7,22 @@ public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> lists = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        boolean[] selected = new boolean[nums.length];
-        dfs(0, nums, selected, list, lists);
+        boolean[] isSelected = new boolean[nums.length];
+        dfs(0, nums, isSelected, list, lists);
         return lists;
     }
-    private void dfs(int u, int[] nums, boolean[] selected, List<Integer> list, List<List<Integer>> lists) {
+    private void dfs(int u, int[] nums, boolean[] isSelected, List<Integer> list, List<List<Integer>> lists) {
         if (u == nums.length) {
             lists.add(new ArrayList<>(list));
             return ;
         }
         for (int i = 0; i < nums.length; i ++) {
-            if (!selected[i]) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
                 list.add(nums[i]);
-                selected[i] = true;
-                dfs(u + 1, nums, selected, list, lists);
+                dfs(u + 1, nums, isSelected, list, lists);
                 list.remove(list.size() - 1);
-                selected[i] = false;
+                isSelected[i] = false;
             }
         }
     }
