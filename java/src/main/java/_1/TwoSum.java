8e882b7ef16974d9a8ab148fc556fe3c356c5b35
linj2n <linj2n@163.com>
2019-02-16 00:06:07 +0800

    🐛 Fix a bug
    NONE #31

--- java/src/main/java/_1/TwoSum.java
+++ java/src/main/java/_1/TwoSum.java
@@ -6,15 +6,16 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         return calculByDFS(nums, target);
     }
+    // solution 1
     private int[] calculByDFS(int[] nums, int target) {
         int[] res = new int[2];
         boolean[] used = new boolean[nums.length];
-        dfs(nums, 0, target, used, res);
+        dfs(nums, 0, 0, target, used, res);
         return res;
     }
-    private void dfs(int[] nums, int u, int target, boolean[] used, int[] res) {
-        if (u == nums.length) {
-            if (target == 0) {
+    private void dfs(int[] nums, int u, int count, int target, boolean[] used, int[] res) {
+        if (u == nums.length || count == 2) {
+            if (target == 0 && count == 2) {
                 for (int i = 0, k = 0; i < used.length; i ++) {
                     if (used[i]) {
                         res[k ++] = i;
@@ -24,11 +25,12 @@ public class TwoSum {
             return ;
         }
         used[u] = true;
-        dfs(nums, u + 1, target - nums[u], used, res);
+        dfs(nums, u + 1, count + 1, target - nums[u], used, res);
         used[u] = false;
-        dfs(nums, u + 1, target, used, res);
+        dfs(nums, u + 1, count, target, used, res);
 
     }
+    // solution 2
     private int[] usingMap(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
