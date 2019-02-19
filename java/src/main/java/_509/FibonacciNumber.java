c3a04264fba63b70d6e21da048a31aaefbc943d9
linj2n <linj2n@163.com>
2019-02-19 12:11:24 +0800

    ✨ Add a new solution
    NONE #31

--- java/src/main/java/_509/FibonacciNumber.java
+++ java/src/main/java/_509/FibonacciNumber.java
@@ -1,6 +1,12 @@
 package _509;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FibonacciNumber {
+
+    private Map<Integer, Integer> map = new HashMap<>();
+
     public int fib(int N) {
         return solution1(N);
     }
@@ -13,6 +19,18 @@ public class FibonacciNumber {
     }
 
     private int solution2(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int res = solution2(n - 1) + solution2(n - 2);
+        map.put(n, res);
+        return res;
+    }
+
+    private int solution3(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
