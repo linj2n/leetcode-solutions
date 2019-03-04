a33484aa17df28c98d9aeaaba6a6076f4e54b671
linj2n <linj2n@163.com>
2019-03-04 13:42:26 +0800

    🐛 Remove the wrong solution
    NONE #31

--- java/src/main/java/_160_InterSectionOfTwoLinkedLists/Solution.java
+++ java/src/main/java/_160_InterSectionOfTwoLinkedLists/Solution.java
@@ -1,13 +1,13 @@
-package _160;
+package _160_InterSectionOfTwoLinkedLists;
 
 import support.ListNode;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class IntersectionOfTwoLinkedLists {
+public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return solution3(headA, headB);
+        return solution2(headA, headB);
     }
     private ListNode solution1(ListNode headA, ListNode headB) {
         Set<ListNode> set = new HashSet<>();
@@ -26,26 +26,6 @@ public class IntersectionOfTwoLinkedLists {
         return null;
     }
     private ListNode solution2(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null || headA.next == null || headB.next == null) {
-            return null;
-        }
-        ListNode curr = headA;
-        while (curr.next != null) {
-            curr = curr.next;
-        }
-        curr.next = headA;
-
-        ListNode low = headB, fast = headB;
-        do {
-            if (fast.next == null || fast.next.next == null) {
-                return null;
-            }
-            low = low.next;
-            fast = fast.next.next;
-        } while (low != fast);
-        return low.next;
-    }
-    private ListNode solution3(ListNode headA, ListNode headB) {
         if(headA == null || headB == null){
             return null;
         }
