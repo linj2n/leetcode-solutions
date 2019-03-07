4b53a887a164fa214d1589ec11dec8211870511e
linj2n <linj2n@163.com>
2019-03-07 23:26:53 +0800

    ✨ Rename and add the new solutions
    NONE #31

--- java/src/main/java/_268_MissingNumber/Solution.java
+++ java/src/main/java/_268_MissingNumber/Solution.java
@@ -1,15 +1,16 @@
-package _268;
+package _268_MissingNumber;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MissingNumber {
+public class Solution {
     public int missingNumber(int[] nums) {
-        return solution3(nums);
+        return solution4(nums);
     }
 
     // solution 1: Sorting
+    // Time: O(nlogn), Space: O(n)
     private int solution1(int[] nums) {
         Arrays.sort(nums);
         int i = 0;
@@ -23,6 +24,7 @@ public class MissingNumber {
     }
 
     // solution 2: Using set
+    // Time: O(n), Space: O(n)
     private int solution2(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -38,11 +40,22 @@ public class MissingNumber {
     }
 
     // solution 3: Bit manipulation
+    // Time: O(n), space: O(1)
     private int solution3(int[] nums) {
         int missing = nums.length;
         for (int i = 0; i < nums.length; i ++) {
             missing = (missing ^ i ^ nums[i]);
         }
         return missing;
+    }
+
+    // solution 4: Sum
+    // Time: O(n), space: O(1)
+    private int solution4(int[] nums) {
+        int sum = nums.length * (nums.length + 1) / 2;
+        for (int n : nums){
+            sum -= n;
+        }
+        return sum;
     }
 }
