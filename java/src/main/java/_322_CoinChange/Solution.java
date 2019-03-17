af49b0b011a5baba6f5f325ab3056712f67ae82b
linj2n <linj2n@163.com>
2019-03-17 16:57:56 +0800

    ✨ Add the new DP solution and some comments
    NONE #31

--- java/src/main/java/_322_CoinChange/Solution.java
+++ java/src/main/java/_322_CoinChange/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public int coinChange(int[] coins, int amount) {
         return solution3(coins, amount);
     }
-    // Solution1: dfs (TLE)
+    // Solution 1: Brute force (TLE)
     // Time: O(2^n), Space: O(n)
     private int dfs(int i, int[] coins, int amount) {
         if (amount == 0) {
@@ -27,7 +27,7 @@ public class Solution {
         }
         return -1;
     }
-    // Solution2: top-Down-DP (AC)
+    // Solution 2: Memoization
     // Time: O(amount * n), Space: O(amount)
     private int solution2(int i, int[] coins, int amount) {
         // return calc2(0, coins, amount, new int[coins.length + 1][amount + 1]);
@@ -72,7 +72,7 @@ public class Solution {
         return dp[i][amount];
     }
 
-    // Solution3: bottom-up DP
+    // Solution 3: Dynamic programming - Bottom up (1)
     // Time: O(n * amount), Space: O(amount)
     private int solution3(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
@@ -86,5 +86,41 @@ public class Solution {
             }
         }
         return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    // Solution 4: Dynamic programming - Bottom up (2)
+    private int solution4(int[] coins, int amount) {
+        if (coins == null || amount <= 0) {
+            return 0;
+        }
+        if (coins.length == 0) {
+            return -1;
+        }
+        // 0. dp[i][j] 表示：使用 coins[0..i] 的硬币组成 j 元需要硬币的最小数目
+        int[][] dp = new int[coins.length][amount + 1];
+        for (int t = 0; t < dp.length; t ++) {
+            Arrays.fill(dp[t], Integer.MAX_VALUE);
+        }
+        // 1. 问题边界状态： dp[0][k * coins[0]] = k, dp[0..r][0] = 0
+        for (int k = 0; k * coins[0] <= amount; k ++) {
+            dp[0][k * coins[0]] = k;
+        }
+        for (int r = 0; r < coins.length; r ++) {
+            dp[r][0] = 0;
+        }
+        // 2. 从问题边界出发，递推地求解, 状态转移方程： dp[i][j] = min{dp[i-1][j], dp[i][j - coins[i]] + 1}
+        for (int i = 1; i < coins.length; i ++) {
+            for (int j = 1; j <= amount; j ++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
+                if (j - coins[i] >= 0 && dp[i][j - coins[i]] != Integer.MAX_VALUE ) {
+                    dp[i][j] =  Math.min(dp[i][j], dp[i][j - coins[i]] + 1);
+                }
+            }
+        }
+        int res = dp[coins.length - 1][amount];
+        if (res == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return res;
     }
 }
