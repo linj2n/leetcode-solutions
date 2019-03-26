4c6809e44c12b23a2f95c2c4b2c217a0ffdff4fa
linj2n <linj2n@163.com>
2019-03-26 22:59:56 +0800

    ✨ Rename and add the new solution
    NONE #31

--- java/src/main/java/_343_IntegerBreak/Solution.java
+++ java/src/main/java/_343_IntegerBreak/Solution.java
@@ -1,6 +1,6 @@
-package _343;
+package _343_IntegerBreak;
 
-public class IntegerBreak {
+public class Solution {
     public int integerBreak(int length) {
         return solution1(length);
     }
@@ -24,5 +24,16 @@ public class IntegerBreak {
             length -= 3;
         }
         return res;
+    }
+    private int solution2(int n) {
+        return calc(0, n, new int[n + 1][n + 1]);   
+    }
+    private int calc(int i, int n, int[][] dp) {
+        int res = Integer.MIN_VALUE;
+        if (i == n) {
+            res = n == 0;    
+        }
+        dp[i][n] = res == Integer.MIN_VALUE ? -1 : res;
+        return dp[i][n];
     }
 }
