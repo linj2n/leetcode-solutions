3ecf097df7f8e4b0bede35aa954a4ca16ea690d4
linj2n <linj2n@163.com>
2020-01-02 20:45:53 +0800

    🚧 Add new Solution
    NONE #31

--- java/src/main/java/_90_Subsets_II/SubsetsII.java
+++ java/src/main/java/_90_Subsets_II/SubsetsII.java
@@ -1,12 +1,17 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
+        List<Integer> path = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        dfs(new ArrayList<>(), 0, nums, ans);
-        return ans;
-    }
+        if (nums == null) {
+            ans.add(path);
+            return ans;
+        }
+        Arrays.sort(nums);
+        // dfs_1(path, 0, nums, ans);
+        dfs_2(path, 0, nums, ans);
+        return ans;    }
     
-    private void dfs(List<Integer> list, int i, int[] nums, List<List<Integer>> ans) {
+    private void dfs_1(List<Integer> list, int i, int[] nums, List<List<Integer>> ans) {
         if (i == nums.length) {
             ans.add(new ArrayList<>(list));
             return ;
@@ -15,12 +20,26 @@ class Solution {
         while (k < nums.length && nums[k] == nums[i]) {
             ++ k;
         }
+        dfs(list, k, nums, ans);
         for (int t = i; t < k; t ++) {
-            dfs(list, t + 1, nums, ans);
             list.add(nums[t]);
+            dfs(list, k, nums, ans);
         }
-        for (int s = 0; s < k - i + 1; s ++) {
+        for (int s = 0; s < k - i; s ++) {
             list.remove(list.size() - 1);
         }
     }
+
+    private void dfs_2(List<Integer> path, int u, int[] nums, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(path));
+        for (int i = u; i < nums.length; i ++) {
+            if (i > u && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            dfs(path, i + 1, nums, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+
 }
