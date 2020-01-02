dcee7cc152374b11188d52c18c189c8cab99d8ba
linj2n <linj2n@163.com>
2020-01-02 20:46:14 +0800

    🚧 Add new Solution
    NONE #31

--- java/src/main/java/_40/CombinationSumII.java
+++ java/src/main/java/_40/CombinationSumII.java
@@ -6,31 +6,57 @@ import java.util.List;
 
 public class CombinationSumII {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> lists = new ArrayList<>();
-        if (candidates == null || candidates.length == 0) {
-            return lists;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        if (candidates == null) {
+            return ans;
         }
         Arrays.sort(candidates);
-        dfs(0, 0, target, candidates, lists, new ArrayList<>());
-        return lists;
+        dfs(path, 0, 0, candidates, target, ans);
+        backtracking(path, 0, 0, target, candidates, ans);
+        return ans;    
     }
-    private void dfs(int i, int sum, int target, int[] candidates, List<List<Integer>> lists, List<Integer> list) {
-        if (sum > target) {
-            return ;
-        }
+    private void dfs(List<Integer> path, int sum, int start, int[] candidates, int target, List<List<Integer>> ans) {
         if (sum == target) {
-            lists.add(new ArrayList<>(list));
+            ans.add(new ArrayList<>(path));
             return ;
         }
-        for (int t = i; t < candidates.length; t ++) {
-            if(t > i && candidates[t] == candidates[t - 1]) {
-                continue;  // skip duplicates
+        if (start == candidates.length || sum > target) {
+            return ;
+        }
+        
+        for (int i = start; i < candidates.length; i ++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
             }
-            sum += candidates[t];
-            list.add(candidates[t]);
-            dfs(t + 1, sum, target, candidates, lists, list);
+            path.add(candidates[i]);
+            dfs(path, sum + candidates[i], i + 1, candidates, target, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    private void backtracking(List<Integer> list, int i, int sum, int target, int[] candidates, List<List<Integer>> ans) {
+        if (sum == target) {
+            ans.add(new ArrayList<>(list));
+            return ;
+        }
+        if (i == candidates.length || sum > target) {
+            return ;
+        }
+        int u = i; 
+        while (u < candidates.length && candidates[u] == candidates[i]) {
+            u ++;
+        }
+        
+        int t = 0;
+        for (; t <= u - i; t ++) {
+            backtracking(list, u, sum + t * candidates[i], target, candidates, ans);
+            list.add(candidates[i]);
+        }
+        
+        while (t != 0) {
             list.remove(list.size() - 1);
-            sum -= candidates[t];
+            t --;
         }
     }
 }
