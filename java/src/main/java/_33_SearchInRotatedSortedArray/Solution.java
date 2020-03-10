b5d248117af419050f3a207b2382085f3f4d8968
linj2n <linj2n@163.com>
2020-03-10 12:32:05 +0800

    🎨 Improve format of the code.
    NONE #31

--- java/src/main/java/_33_SearchInRotatedSortedArray/Solution.java
+++ java/src/main/java/_33_SearchInRotatedSortedArray/Solution.java
@@ -1,36 +1,37 @@
 package _33_SearchInRotatedSortedArray;
 
 public class Solution {
+
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-        int l = 0, r = nums.length - 1, mid = 0;
-        if (nums[0] > nums[r]) {
+        int l = 0, r = nums.length - 1, delta = 0, mid = 0;
+        if (nums[0] > nums[nums.length - 1]) {
+            // find the pivot
+            int firstElem = nums[0];
             while (l < r) {
                 mid = l + (r - l) / 2;
-                if (nums[mid] < nums[0]) {
+                if (nums[mid] < firstElem) {
                     r = mid;
                 } else {
                     l = mid + 1;
                 }
             }
+            // update delta with pivot;
+            delta = l;
         }
-        int k = l;
-        l = 0; r = nums.length - 1;
+        // find the smallest element in the list that is larger than or equal to the given target with Delta.
+        l = 0;
+        r = nums.length - 1;
         while (l < r) {
-            mid = l + (r - l) / 2 ;
-            int realMid = (mid + k) % nums.length;
-            if (nums[realMid] >= target) {
+            mid = l + (r - l) / 2;
+            if (nums[(mid + delta) % nums.length] >= target) {
                 r = mid;
             } else {
                 l = mid + 1;
             }
         }
-        int res = (l + k) % nums.length;
-        if (nums[res] != target) {
-            return -1;
-        }
-        return res;
+        return target == nums[(l + delta) % nums.length] ? (l + delta) % nums.length : - 1;
     }
 }
