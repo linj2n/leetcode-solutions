5c10de3b6abd62f686f2b9f7ca886c8a1d36034b
linj2n <linj2n@163.com>
2020-03-20 14:34:23 +0800

    ♻️ Refactoring code
    NONE #31

--- java/src/main/java/_26/RemoveDuplicatesfromSortedArray.java
+++ java/src/main/java/_26/RemoveDuplicatesfromSortedArray.java
@@ -2,12 +2,16 @@ package _26;
 
 public class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
-        int l = 0, r = 0;
+        if (nums.length < 1) {
+            return nums.length;
+        }
+        int l = 0, r = 1;
         while (r < nums.length) {
-            if (r == 0 || nums[r] != nums[r - 1]) {
-                nums[l ++] = nums[r];
+            if (nums[r] != nums[l]) {
+                nums[++ l] = nums[r];
             }
             r ++;
         }
-        return l;    }
+        return l + 1;    
+    }    
 }
